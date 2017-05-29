8ca37fcea6e578eb846933d2871c701117e5d333
KL Tah <kltah@gmail.com>
2017-05-29 09:55:45 +0800

    8. middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Wes';
+  res.cookie('name', 'chocolate chip', {maxAge:9999});
+  if (req.name === 'Wes') {
+    throw Error ('that is a stupid name')
+  }
+  next()
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
